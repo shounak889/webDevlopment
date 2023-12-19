03f3ee6123e839d5cee1dde7cc312d045505e314
shounak889 <shounakrendalkar@gmail.com>
2023-12-20 00:40:30 +0530

    Created component "ram" and displayed

---
+++ Angular/TestProject/src/app/ram/ram.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ram',
+  standalone: true,
+  imports: [],
+  templateUrl: './ram.component.html',
+  styleUrl: './ram.component.css'
+})
+export class RamComponent {
+
+}
