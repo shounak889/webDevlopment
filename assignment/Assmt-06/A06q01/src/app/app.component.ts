8932cf7848cf3d3b6d6434fa5cdd38d3181a5075
shounak889 <shounakrendalkar@gmail.com>
2023-12-28 23:57:44 +0530

    solving A06q01

---
+++ assignment/Assmt-06/A06q01/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+
+  public in1 : string = "";
+
+}
