63077a04cff02512aba892ccbd60cb38891ffcfb
shounak889 <shounakrendalkar@gmail.com>
2024-01-04 01:35:54 +0530

    A06q01 completed

--- assignment/Assmt-06/A06q01/src/app/app.component.ts
+++ assignment/Assmt-06/A06q01/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-
-  public in1 : string = "";
-
+  public n1 : string = "";
 }
