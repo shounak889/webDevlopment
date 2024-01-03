fc89db8d01dafc87900b729bc680a7189f7948a3
shounak889 <shounakrendalkar@gmail.com>
2024-01-04 02:32:16 +0530

    A06q03 Inline css and html

--- assignment/Assmt-06/A06q03/src/app/app.component.ts
+++ assignment/Assmt-06/A06q03/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+
+// here we have used th `` tilt symbol above the tab key  and didi the inline css and html insted of giving the path for it 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <h1 class="dj">
+    Marvellous Infosystems</h1>
+  
+  <h1 class="bravo">
+    Enter something that is in your mind</h1>
+  
+  <input [(ngModel)] = "n3" type="text">
+  
+  <h1 class="ok">
+    You are typing ::: {{n3}}</h1>
+  
+  <router-outlet></router-outlet>
+  `,
+  styles: `.dj
+  {
+      color: blue;
+  }
+  
+  .bravo
+  {
+      color: darkturquoise;
+  }
+  
+  .ok
+  {
+      color: lightseagreen;
+  }`
 })
 export class AppComponent {
-  title = 'A06q03';
+  public n3 = "";
 }
